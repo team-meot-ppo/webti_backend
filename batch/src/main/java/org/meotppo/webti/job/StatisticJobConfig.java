ce41481f80e2aaedea24bbdaf1d8021b39c03df6
jeyong <joon6093@naver.com>
2024-07-02 07:49:53 +0900

    feat : MongoDB Aggregation을 이용하여 Batch 작업 성능 개선

--- batch/src/main/java/org/meotppo/webti/job/StatisticJobConfig.java
+++ batch/src/main/java/org/meotppo/webti/job/StatisticJobConfig.java
@@ -1,28 +1,20 @@
 package org.meotppo.webti.job;
 
-import org.meotppo.webti.domain.entity.jpa.statistics.Statistic;
-import org.meotppo.webti.domain.entity.mongo.testresult.TestResult;
+import org.meotppo.webti.job.tasklet.StatisticTasklet;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.data.MongoPagingItemReader;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import static org.meotppo.webti.config.TransactionManagerConfig.DOMAIN_TRANSACTION_MANAGER;
-import static org.meotppo.webti.job.processor.ProcessorConfig.STATISTIC_PROCESSOR;
-import static org.meotppo.webti.job.reader.ReaderConfig.TEST_RESULT_READER;
-import static org.meotppo.webti.job.writer.WriterConfig.STATISTIC_WRITER;
 
 @Configuration
-public class StatisticJobConfig {  // TODO. 통계 로직의 성능 개선
+public class StatisticJobConfig {
 
     public static final String STATISTIC_JOB = "statisticJob";
     public static final String STATISTIC_STEP = "statisticStep";
@@ -35,24 +27,38 @@ public class StatisticJobConfig {  // TODO. 통계 로직의 성능 개선
         this.domainTransactionManager = domainTransactionManager;
     }
 
-    @Bean(name = STATISTIC_JOB)
+//    @Bean(name = STATISTIC_JOB)  // 기존 Job
+//    public Job statisticJob(@Qualifier(STATISTIC_STEP) Step statisticStep) {
+//        return new JobBuilder(STATISTIC_JOB, jobRepository)
+//                .incrementer(new RunIdIncrementer())
+//                .start(statisticStep)
+//                .build();
+//    }
+//
+//    @Bean(name = STATISTIC_STEP)
+//    public Step statisticStep(
+//            @Qualifier(TEST_RESULT_READER) MongoPagingItemReader<TestResult> testResultReader,
+//            @Qualifier(STATISTIC_PROCESSOR) ItemProcessor<TestResult, Statistic> statisticProcessor,
+//            @Qualifier(STATISTIC_WRITER) ItemWriter<Statistic> statisticWriter) {
+//        return new StepBuilder(STATISTIC_STEP, jobRepository)
+//                .<TestResult, Statistic>chunk(10, domainTransactionManager)
+//                .reader(testResultReader)
+//                .processor(statisticProcessor)
+//                .writer(statisticWriter)
+//                .build();
+//    }
+
+    @Bean(name = STATISTIC_JOB)  // 개선 Job
     public Job statisticJob(@Qualifier(STATISTIC_STEP) Step statisticStep) {
         return new JobBuilder(STATISTIC_JOB, jobRepository)
-                .incrementer(new RunIdIncrementer())
                 .start(statisticStep)
                 .build();
     }
 
     @Bean(name = STATISTIC_STEP)
-    public Step statisticStep(
-            @Qualifier(TEST_RESULT_READER) MongoPagingItemReader<TestResult> testResultReader,
-            @Qualifier(STATISTIC_PROCESSOR) ItemProcessor<TestResult, Statistic> statisticProcessor,
-            @Qualifier(STATISTIC_WRITER) ItemWriter<Statistic> statisticWriter) {
+    public Step statisticStep(StatisticTasklet statisticTasklet) {
         return new StepBuilder(STATISTIC_STEP, jobRepository)
-                .<TestResult, Statistic>chunk(10, domainTransactionManager)
-                .reader(testResultReader)
-                .processor(statisticProcessor)
-                .writer(statisticWriter)
+                .tasklet(statisticTasklet, domainTransactionManager)
                 .build();
     }
 }
--- domain/src/main/java/org/meotppo/webti/domain/config/mongo/MongoConfig.java
+++ domain/src/main/java/org/meotppo/webti/domain/config/mongo/MongoConfig.java
@@ -2,7 +2,10 @@ package org.meotppo.webti.domain.config.mongo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
 import java.util.List;
 
 @Configuration
@@ -13,5 +16,10 @@ public class MongoConfig {
             LocalDateTimeToDateKstConverter localDateTimeToDateKstConverter,
             DateToLocalDateTimeKstConverter dateToLocalDateTimeKstConverter) {
         return new MongoCustomConversions(List.of(localDateTimeToDateKstConverter, dateToLocalDateTimeKstConverter));
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDbFactory) {
+        return new MongoTemplate(mongoDbFactory);
     }
 }
