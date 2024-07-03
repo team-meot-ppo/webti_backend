42d1bec4f23fc1e97c4aba390830589537f52bba
Kang Dong Hyeon <ehdgus6435@kumoh.ac.kr>
2024-07-03 22:07:21 +0900

    fix : requestdto에 JsonProperty 제거

--- api/src/main/java/org/meotppo/webti/dto/propensityanalysis/PropensityAnalysisDto.java
+++ api/src/main/java/org/meotppo/webti/dto/propensityanalysis/PropensityAnalysisDto.java
@@ -1,34 +1,24 @@
 package org.meotppo.webti.dto.propensityanalysis;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class PropensityAnalysisDto {
     @NotNull(message = "EXTROVERSION cannot be null")
-    @JsonProperty("EXTROVERSION")
     private int EXTROVERSION;
     @NotNull(message = "INTROVERSION cannot be null")
-    @JsonProperty("INTROVERSION")
     private int INTROVERSION;
     @NotNull(message = "INTUITION cannot be null")
-    @JsonProperty("INTUITION")
     private int INTUITION;
     @NotNull(message = "SENSING cannot be null")
-    @JsonProperty("SENSING")
     private int SENSING;
     @NotNull(message = "THINKING cannot be null")
-    @JsonProperty("THINKING")
     private int THINKING;
     @NotNull(message = "FEELING cannot be null")
-    @JsonProperty("FEELING")
     private int FEELING;
     @NotNull(message = "PERCEIVING cannot be null")
-    @JsonProperty("PERCEIVING")
     private int PERCEIVING;
     @NotNull(message = "JUDGING cannot be null")
-    @JsonProperty("JUDGING")
     private int JUDGING;
 }
