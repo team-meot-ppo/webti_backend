5fe3efccb987430e389b776cf80f99e10a909b91
jeyong <joon6093@naver.com>
2024-09-01 04:43:41 +0900

    test : mongo를 사용하는 TestResultRepository 테스트 코드 작성

---
+++ batch/src/test/java/org/meotppo/webti/TestResultRepositoryTest.java
@@ -0,0 +1,73 @@
+package org.meotppo.webti;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.meotppo.webti.config.MongoTest;
+import org.meotppo.webti.domain.entity.mongo.testresult.TestResult;
+import org.meotppo.webti.domain.entity.type.MbtiType;
+import org.meotppo.webti.domain.repository.mongo.testresult.TestResultRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@MongoTest
+@SpringBootTest
+@ActiveProfiles(value = "test")
+public class TestResultRepositoryTest {
+
+    @Autowired
+    private TestResultRepository testResultRepository;
+
+    @BeforeEach
+    void setUp() {
+        testResultRepository.deleteAll();
+    }
+
+    @Test
+    void testSave() {
+        // given
+        TestResult result = TestResult.builder()
+                .mbtiType(MbtiType.INFJ)
+                .match(true)
+                .build();
+
+        // when
+        testResultRepository.save(result);
+        TestResult foundResult = testResultRepository.findById(result.getId()).get();
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(foundResult.getId()).isEqualTo(result.getId());
+            softly.assertThat(foundResult.getMbtiType()).isEqualTo(result.getMbtiType());
+            softly.assertThat(foundResult.isMatch()).isEqualTo(result.isMatch());
+        });
+    }
+
+    @Test
+    void testDeleteAll() {
+        // given
+        TestResult result1 = TestResult.builder()
+                .mbtiType(MbtiType.INFJ)
+                .match(true)
+                .build();
+
+        TestResult result2 = TestResult.builder()
+                .mbtiType(MbtiType.ENTP)
+                .match(false)
+                .build();
+
+        testResultRepository.save(result1);
+        testResultRepository.save(result2);
+
+        // when
+        assertSoftly(softly -> {
+            softly.assertThat(testResultRepository.findAll()).hasSize(2);
+            testResultRepository.deleteAll();
+
+            // then
+            softly.assertThat(testResultRepository.findAll()).isEmpty();
+        });
+    }
+}
