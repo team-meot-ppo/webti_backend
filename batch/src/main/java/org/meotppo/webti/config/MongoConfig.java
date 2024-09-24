6926b33db06fdd7174a20d52f36b7fbb3fd785ea
jeyong <joon6093@naver.com>
2024-09-24 23:05:02 +0900

    refactor : api와 batch에서 불필요한 Mongo 설정 중복 제거

--- batch/src/main/java/org/meotppo/webti/config/MongoConfig.java
+++
@@ -1,9 +0,0 @@
-package org.meotppo.webti.config;
-
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-@EnableMongoAuditing
-@EnableMongoRepositories(basePackages = "org.meotppo.webti.domain.repository.mongo")
-public class MongoConfig {
-}
